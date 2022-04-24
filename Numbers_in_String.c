f9cadb23494f69a098244213dc59d63f48143650
sadwika6 <97425809+sadwika6@users.noreply.github.com>
2022-04-24 10:56:42 +0530

    #include<stdio.h> int main() {     char str[100];     int i,nc=0,sum;     scanf("%[^' ']s",str);     for(i=0;str[i]!=NULL;i++)     {         if((str[i]>='0')&&(str[i]<='9'))         {             nc++;             sum +=(str[i]-'0');         }     }     printf("%d ",sum); }

---
+++ Numbers_in_String.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    int i,nc=0,sum;
+    scanf("%[^'
+']s",str);
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if((str[i]>='0')&&(str[i]<='9'))
+        {
+            nc++;
+            sum +=(str[i]-'0');
+        }
+    }
+    printf("%d
+",sum);
+}
