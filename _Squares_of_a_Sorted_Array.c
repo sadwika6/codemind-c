9b3ed74d1ea9757c268740a7815d4b3f154ca16d
sadwika6 <97425809+sadwika6@users.noreply.github.com>
2022-04-24 15:24:10 +0530

    #include<stdio.h> int main() {     int n,i,j,arr[100],temp=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         arr[i]=arr[i]*arr[i];     }     for(i=0;i<n;i++)     {         for(j=i+1;j<n;j++)         {             if(arr[i]>arr[j])             {                 temp=arr[i];                 arr[i]=arr[j];                 arr[j]=temp;             }         }         printf("%d ",arr[i]);     } }

---
+++ _Squares_of_a_Sorted_Array.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,arr[100],temp=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        arr[i]=arr[i]*arr[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        printf("%d ",arr[i]);
+    }
+}
