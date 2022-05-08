3c920537a52aa9c2c3dab640badc3af78c1c23a7
sadwika6 <97425809+sadwika6@users.noreply.github.com>
2022-05-08 09:25:34 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=n;i>0;i--)     {         for(j=1;j<=i;j++)         {             printf("%c ",'A'-1+i);         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,18 +3,11 @@ int main()
 {
     int n,i,j;
     scanf("%d",&n);
-    for(i=0;i<n;i++)
+    for(i=n;i>0;i--)
     {
-        for(j=0;j<n;j++)
+        for(j=1;j<=i;j++)
         {
-            if(j==0 || j==n-1 || i==j)
-            {
-                printf("* ");
-            }
-            else
-            {
-                printf("  ");
-            }
+            printf("%c ",'A'-1+i);
         }
         printf("
 ");
