73b236a50e04516d9f68b22c77d5e52887eb2e49
sadwika6 <97425809+sadwika6@users.noreply.github.com>
2022-05-17 19:26:18 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,a,k=0,b;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {         if(arr[i]>=a&&arr[i]<=b)        // sum+=arr[i];        printf("%d ",arr[i]);        else        k++;     }     if(k==n)     printf("-1");    // printf("%d",sum); }

---
+++ elements_in_between_A_and_B.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,a,k=0,b;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>=a&&arr[i]<=b)
+       // sum+=arr[i];
+       printf("%d ",arr[i]);
+       else
+       k++;
+    }
+    if(k==n)
+    printf("-1");
+   // printf("%d",sum);
+}
