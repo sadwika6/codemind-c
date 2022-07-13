f4d22f774f4b7638c26d7d7ca28da5da26086e03
sadwika6 <97425809+sadwika6@users.noreply.github.com>
2022-07-13 20:54:36 +0530

    #include<stdio.h> int prime(int n) {     int i;     if(n==1)     {         return 0;     }     for(i=2;i<=(n/2)+1;i++)     {         if(n%i==0)         {             return 0;         }     }     return 1; } int main() {     int t,i;     scanf("%d",&t);     for(i=0;i<t;i++)     {         int n;         scanf("%d",&n);         int x,y,i;         for(i=n;i>1;i--)         {             if(prime(i))             {                 x=i;                 break;             }         }         for(i=n;;i++)         {             if(prime(i))             {                 y=i;                 break;             }         }         if(n-x<=y-n)         {             printf("%d ",x);         }         else         {             printf("%d ",y);         }     } }

---
+++ Nearest_Prime.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+int prime(int n)
+{
+    int i;
+    if(n==1)
+    {
+        return 0;
+    }
+    for(i=2;i<=(n/2)+1;i++)
+    {
+        if(n%i==0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+int main()
+{
+    int t,i;
+    scanf("%d",&t);
+    for(i=0;i<t;i++)
+    {
+        int n;
+        scanf("%d",&n);
+        int x,y,i;
+        for(i=n;i>1;i--)
+        {
+            if(prime(i))
+            {
+                x=i;
+                break;
+            }
+        }
+        for(i=n;;i++)
+        {
+            if(prime(i))
+            {
+                y=i;
+                break;
+            }
+        }
+        if(n-x<=y-n)
+        {
+            printf("%d
+",x);
+        }
+        else
+        {
+            printf("%d
+",y);
+        }
+    }
+}
